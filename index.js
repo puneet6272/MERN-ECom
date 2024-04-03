391f1bff789e7deda26f597579111d1eaceee9ad
Puneet <koliarun323@gmail.com>
2024-04-04 02:39:51 +0530

    Can ignorable

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ server.use(express.static(path.resolve(__dirname,'build')))
 server.use(cookieParser());
 server.use(
   session({
-    secret: 'keyboard cat',
+    secret: 'SESSION_KEY',
     resave: false, // don't save session if unmodified
     saveUninitialized: false, // don't create session until something stored
   })
